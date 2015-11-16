654e672c387aadf627354d9b3949355b3dc67727
Ilya Domrachev <twenty_box@mail.ru>
2015-11-16 23:17:40 +0300

    Добавил api для всех нужных таблиц БД

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
 var express = require('express'),
 		app = express(),
-		db = require('./server/firebird'),
+		db = require('./server/queries'),
 		fs = require('fs');
 
 app.use(express.static(__dirname + '/views'));
 
-var classes = db.queryDB('select * from chem_class');
-
 app.get('/classes', function (req, res) {
     res.setHeader('Content-Type', 'application/json');
-    res.send(classes);
+    res.send(db.classesData);
 });
 
+app.get('/products', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(db.productsData);
+});
+
+app.get('/params', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(db.paramsData);
+});
+
+app.get('/ei', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(db.eiData);
+});
+
+app.get('/en', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(db.enData);
+});
 
 app.listen(8080);
 
 console.log("Сервер стартовал!");
-
-
