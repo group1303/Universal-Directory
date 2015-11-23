281c2b15bbcf8a15761d15c39f0c2d0573a3cd1d
Ilya Domrachev <twenty_box@mail.ru>
2015-11-24 00:12:40 +0300

    Added queryBLOB
    queryBLOB для чтения BLOB из базы данных

--- server.js
+++ server.js
@@ -18,9 +18,8 @@ var classes = 'SELECT id_class,short_name,name,main_class,base_ei FROM chem_clas
     params = 'SELECT id_par,short_name,name,ei_par,type_par FROM parametr;',
     ei = 'SELECT id_ei,short_name,name,code FROM ei;',
     en = 'SELECT id_enum,short_name,name FROM enum;',
-    //nav = 'select cc.id_class as "id",cc.main_class as "parentId",cc.name as "text",case gg.OTERM when 0 then \'true\' else \'false\' end as "expanded" from chem_class cc, (select * from find_gr_gr(2)) gg where cc.id_class = gg.oidgr';
-    //nav = 'select id_class as "id",main_class as "parentId",name as "text" from chem_class',
-    nav = 'select id_class,main_class,name from chem_class';
+    nav = 'select id_class,main_class,name from chem_class',
+    classesImg = 'select id_class, image from chem_class;';
 /**
  * query
  * Выполняет запрос к базе данных
@@ -40,16 +39,23 @@ function query(qrystr, res){
  * queryPr
  * Формирует строку запроса с использованием процедуры и параметров
  * Затем отправляет запрос в бд и передает данные на сервер в формате JSON
- * Использование: queryPar('Название процедуры', ''+id+','+name+','+shName+'',res)
+ * Использование: queryPar('Название процедуры', '*',''+id+','+name+','+shName+'',res)
  * 
  * @param prName Название процедуры
+ * @param select Необходимые поля
  * @param params Параметры для процедуры
  * @param res Передача переменной res для отправки данных на клиент
  */
-function queryPr(prName, params,res){
-		return query('select * from '+ prName + '(' + params + ');',res);
+function queryPr(prName, select, params, res){
+		return query('select ' + select + ' from '+ prName + '(' + params + ');',res);
 }
 
+function queryBLOB(qrystr, res){
+	db.queryBLOB(qrystr, function(data){
+		res.setHeader('Content-Type', 'text/plain');
+		res.send(data);
+	});
+}
 router.get('/nav', function (req, res, next) {
   query(nav, res);
 });
@@ -58,9 +64,12 @@ router.get('/classes', function (req, res, next) {
   query(classes, res);
 });
 
+router.get('/classesImg', function (req, res, next) {
+  queryBLOB(classesImg, res);
+});
 
 router.get('/classes:id', function (req, res) {
-	queryPr('find_list',req.params.id,res)
+	queryPr('find_list', '*',req.params.id,res)
 });
 
 router.get('/products', function (req, res, next) {
