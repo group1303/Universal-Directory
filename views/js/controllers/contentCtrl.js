11d2fca2270453f8f5ebc07c906b1e3c105977d1
Ilya Domrachev <twenty_box@mail.ru>
2015-11-24 00:14:50 +0300

    Рабочий каталог

--- views/js/controllers/contentCtrl.js
+++ views/js/controllers/contentCtrl.js
@@ -2,60 +2,84 @@ angular.module('UniDir.content', ['ngRoute','dx'])
 
 
 
-.controller('contentCtrl', ['$scope','$http', 
-  function($scope, $http) {
+.controller('contentCtrl', ['$scope','$http', '$rootScope', 
+  function($scope, $http, $rootScope) {
 
-$http.get('http://localhost:8080/classes')
-.success(function(data, status, headers, config) {
-    $scope.items = data;
+// $http.get('http://localhost:8080/classes')
+//   .success(function(data, status, headers, config) {
+//       $scope.items = data;
+//   })
+//   .error(function(error, status, headers, config) {
+//        console.log(status);
+//        console.log("Error data_content.js");
+// });
+
+$http.get('http://localhost:8080/classes2')
+    .success(function(data, status, headers, config) {
+        $scope.items = data;
+        //console.log(data);
+    })
+    .error(function(error, status, headers, config) {
+         console.log(status);
+         console.log("Процедура Find_list не нашла данных");
+  });
+
+$scope.$on ("navClassId", function(event, args) {
+  $scope.navClassId = args.id;
+  $http.get('http://localhost:8080/classes' + $scope.navClassId)
+    .success(function(data, status, headers, config) {
+        $scope.items = data;
+        //console.log(data);
+    })
+    .error(function(error, status, headers, config) {
+         console.log(status);
+         console.log("Процедура Find_list не нашла данных");
+  });
+
 })
-.error(function(error, status, headers, config) {
-     console.log(status);
-     console.log("Error data_content.js");
-});
+
+
+  
+
+// Подсчет количества строк в объекте
+// function countCols(obj){
+//   return Object.keys(obj).length;
+// };
 
 $scope.dataGridOptions = {
   bindingOptions: { 
     dataSource: 'items'
   },
   paging: {
-      enabled: false
+      enabled: true,
+      pageSize: 10
   },
-  editing: {
-    editMode: 'row',
-    editEnabled: true,
-    removeEnabled: true,
-    insertEnabled: true,
-    removeConfirmMessage: 
-        "Вы точно хотите удалить запись?"
-  },
+  // editing: {
+  //   editMode: 'row',
+  //   editEnabled: true,
+  //   removeEnabled: true,
+  //   insertEnabled: true,
+  //   removeConfirmMessage: 
+  //       "Вы точно хотите удалить запись?"
+  // },
   selection: {
     mode: 'multiple'
   },
   columns: [
     {
-      dataField: 'ID_CLASS',
+      dataField: 'OIDEL',
       caption: "ID",
       width: 50
     },
     {
-      dataField: 'SHORT_NAME',
-      caption: "Короткое название",
+      dataField: 'ONAME', 
+      caption: "Полное название",
       width: 200
     },
     {
-      dataField: 'NAME', 
-      caption: "Полное название"
-    },
-    {
-      dataField: 'MAIN_CLASS',
+      dataField: 'ONAMECLASS',
       caption: "Родительский класс",
-      width: 200
-    }, 
-    {
-      dataField: 'BASE_EI',
-      caption: 'Ед. измерения',
-      width: 200
+      width: 300
     }
   ]
 }
